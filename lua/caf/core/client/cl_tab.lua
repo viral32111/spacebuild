4fff5cd80f228cb4da501c730662a85591d090a4
viral32111 <19510403+viral32111@users.noreply.github.com>
2019-02-09 21:13:39 +0000

    Changed some labels to make a bit more sense.

--- lua/caf/core/client/cl_tab.lua
+++ lua/caf/core/client/cl_tab.lua
@@ -10,8 +10,8 @@ end)
 
 function CAF_BuildCPanel(cp, toolname, listname, custom)
 	cp:AddControl("CheckBox", {Label = "Don't Weld", Command = toolname .. "_DontWeld"})
-	cp:AddControl("CheckBox", {Label = "Allow welding to world", Command = toolname .. "_AllowWorldWeld"})
-	cp:AddControl("CheckBox", {Label = "Make Frozen", Command = toolname .. "_Frozen"})
+	cp:AddControl("CheckBox", {Label = "Weld to World", Command = toolname .. "_AllowWorldWeld"})
+	cp:AddControl("CheckBox", {Label = "Spawn Frozen", Command = toolname .. "_Frozen"})
 	
 	local ListControl = vgui.Create("CAFControl")
 	cp:AddPanel(ListControl)
