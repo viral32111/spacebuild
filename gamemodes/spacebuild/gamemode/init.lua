3d3921bbaad390a204072ba36653668826014a36
viral32111 <19510403+viral32111@users.noreply.github.com>
2019-02-09 14:56:49 +0000

    Patched the gamemode & Removed testing maps

--- gamemodes/spacebuild/gamemode/init.lua
+++ gamemodes/spacebuild/gamemode/init.lua
@@ -1,10 +1,3 @@
---
--- Created by IntelliJ IDEA.
--- User: Stijn
--- Date: 29/05/2016
--- Time: 11:46
--- To change this template use File | Settings | File Templates.
---
-
-include( "shared.lua" )
-
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
--- gamemodes/spacebuild/gamemode/shared.lua
+++ gamemodes/spacebuild/gamemode/shared.lua
@@ -1,16 +1,8 @@
---
--- Created by IntelliJ IDEA.
--- User: Stijn
--- Date: 29/05/2016
--- Time: 11:45
--- To change this template use File | Settings | File Templates.
---
-
 GM.Name = "Spacebuild"
-GM.Author = "SB Dev Team"
-GM.Email = "N/A"
-GM.Website = "http://spacebuild.github.io/"
+GM.Author = "SB Dev Team & viral32111"
+GM.Email = "mail@viral32111.com"
+GM.Website = "spacebuildmod.com"
+
 GM.SPACEBUILD = true
 
-DeriveGamemode( "sandbox" )
-
+DeriveGamemode("sandbox")
